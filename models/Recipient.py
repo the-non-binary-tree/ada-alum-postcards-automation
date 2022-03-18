1e4365ddc883ff582aa5d3bee2ceaa56dacfaee8
Sophia Le <sophia.hue.le@gmail.com>
2022-03-18 16:54:35 -0700

    Add stakeholder classes

---
+++ models/Recipient.py
@@ -0,0 +1,5 @@
+class Recipient(object):
+    def __init__(self, fullname, pronouns, cohort, homeroom):
+        self.fullname = fullname
+        self.cohort = cohort
+        self.homeroom = homeroom
---
+++ models/Sender.py
@@ -0,0 +1,12 @@
+class Sender(object):
+    def __init__(self, fullname, email, postcardNum, deliveryMethod):
+        self.fullname = fullname
+        self.email = email
+        self.postcardNum = postcardNum
+        self.deliveryMethod = deliveryMethod
+
+    def matchRecipients(self, recipients):
+        if len(recipients) <= self.postcardNum:
+            self.recipients = recipients
+        else:
+            self.recipients = recipients[:self.postcardNum + 1]
