7be866cdc003b80458a48d88e1081cd833d7ed7f
Sophia Le <sophia.hue.le@gmail.com>
2022-03-18 19:32:22 -0700

    Add parse_file function. Adjust Recipient class.

--- models/Recipient.py
+++ models/Recipient.py
@@ -1,5 +1,5 @@
 class Recipient(object):
-    def __init__(self, fullname, pronouns, cohort, homeroom):
+    def __init__(self, fullname, cohort, email):
         self.fullname = fullname
         self.cohort = cohort
-        self.homeroom = homeroom
+        self.__email = email
