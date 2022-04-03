b1c96635759afac8c20935c37e48e608d23aa2db
Sophia Le <sophia.hue.le@gmail.com>
2022-04-02 17:25:00 -0700

    one more commit

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import sys
-from utils.get_recipients import get_all_recipients
+from utils.get_recipients import get_remaining_recipients
+from utils.get_senders import get_all_senders
+from utils.match_sender_recipients import match_all_senders
 import constants
+import csv
+from models.Sender import Sender
+from models.Recipient import Recipient
 
-student_directory = constants.STUDENT_DIRECTORY
-print(student_directory)
-all_students = get_all_recipients(student_directory)
+recipients = get_remaining_recipients()
 
-print(all_students)
+# should be get remaining senders
+senders = get_all_senders()
+
+match_all_senders(senders, recipients)
+
+print(senders)
+
+# export to csv
+HEADERS = ["Full Name", "Email Address", "Number of Postcards", "List of Assigned Students' Names"]
+rows = list()
+for sender in senders:
+    assigned_recipients = ', '.join([recipient.fullname for recipient in sender.recipients])
+    row = [
+        sender.fullname,
+        sender.email,
+        sender.postcardNum,
+        assigned_recipients
+    ]
+    rows.append(row)
+
+with open('C17-Postcard-Matches.csv', 'w') as f: 
+    write = csv.writer(f) 
+    write.writerow(HEADERS) 
+    write.writerows(rows)
